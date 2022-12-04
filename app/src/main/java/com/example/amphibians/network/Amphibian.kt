510c757ccb770a2ff6461e8a94caf67eae23241a
YanuYuhanta <85419868+YanuYuhanta@users.noreply.github.com>
2022-12-04 15:18:43 +0700

    Update Amphibian.kt

--- app/src/main/java/com/example/amphibians/network/Amphibian.kt
+++ app/src/main/java/com/example/amphibians/network/Amphibian.kt
@@ -16,9 +16,8 @@
 package com.example.amphibians.network
 
 /**
- * This data class defines an Amphibian which includes the amphibian's name, the type of
- * amphibian, and a brief description of the amphibian.
- * The property names of this data class are used by Moshi to match the names of values in JSON.
+ * class Amphibian mendefinisikan data Amphibi dengan menyertakan nama dan deskripsinya
+ * properti pada class digunakan moshi untuk mencocokan nama nilai di JSON
  */
 data class Amphibian(
     val name: String,
